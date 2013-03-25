d06107dda0907b8f94c755d1f88c7bd447d9e31c
Nik Karpinsky <karpinsn@iastate.edu>
2013-03-25 16:49:02 -0500

    Adding in projector phase wrapping and unwrapping

---
+++ src/TwoWavelengthPhaseUnwrapper.h
@@ -0,0 +1,14 @@
+#ifndef _PORTAL_CALIBRATION_TWO_WAVELENGTH_PHASE_UNWRAPPER_H_
+#define _PORTAL_CALIBRATION_TWO_WAVELENGTH_PHASE_UNWRAPPER_H_
+
+class PhaseUnwrapper
+{
+    public:
+};
+
+class TwoWavelengthPhaseUnwrapper : public PhaseUnwrapper
+{
+    public:
+};
+
+#endif //_PORTAL_CALIBRATION_TWO_WAVELENGTH_PHASE_UNWRAPPER_H_
