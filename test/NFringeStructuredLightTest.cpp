e46dd7bfcd0f49c178afe8cdd86a26bddaf68e4c
Nik Karpinsky <karpinsn@iastate.edu>
2013-03-28 13:23:03 -0500

    Working on getting tests to link.

--- test/NFringeStructuredLightTest.cpp
+++ test/NFringeStructuredLightTest.cpp
@@ -2,12 +2,12 @@
 
 #include "gtest/gtest.h"
 
-//#include "NFringeStructuredLight.h"
+#include "NFringeStructuredLight.h"
 
 TEST(NFringeStructuredLight, GenerateFringe_CorrectN)
 {
-  //NFringeStructuredLight structuredLight(5);
-  //auto fringe = structuredLight.GenerateFringe(cv::Size(128,128), 70, IStructuredLight::Horizontal);
+  NFringeStructuredLight structuredLight(5);
+  auto fringe = structuredLight.GenerateFringe(cv::Size(128,128), 70, IStructuredLight::Horizontal);
 
-  EXPECT_EQ(1, 1);
+  EXPECT_EQ(fringe.size(), 5);
 }
